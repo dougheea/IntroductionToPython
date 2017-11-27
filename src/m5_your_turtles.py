65650d8470ca61cf5045a62dbb233b25ae5e6e8d
dougheea <dougheea@rose-hulman.edu>
2017-11-27 17:05:17 -0500

    done, finished the turtles

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Emily Dougherty.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,41 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+Elle = rg.SimpleTurtle('turtle')
+Elle.pen = rg.Pen('pink', 2)
+Elle.speed = 20
+
+size1 = 300
+
+Emmett = rg.SimpleTurtle('turtle')
+Emmett.speed = 15
+
+size2 = 15
+
+for k in range(8):
+    Elle.draw_square(size1)
+
+    Elle.pen_up()
+    Elle.right(45)
+    Elle.forward(15)
+    Elle.left(45)
+
+    Elle.pen_down()
+    size1 = (size1 - 15)
+
+for a in range(8):
+    Emmett.draw_circle(size2)
+
+    Emmett.pen_up()
+    Emmett.left(60)
+    Emmett.forward(15)
+    Emmett.right(60)
+
+    Emmett.pen_down()
+    size2 = (size2 + 15)
+
+window.close_on_mouse_click()
